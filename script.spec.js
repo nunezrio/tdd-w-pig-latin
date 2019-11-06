9e320d12a1f66c9a74e3f56d949db2d3da79906a
nunezrio <nunezrio@gmail.com>
2019-11-06 09:15:54 -0500

    added extra tests for words starting with vowels

--- script.spec.js
+++ script.spec.js
@@ -10,9 +10,17 @@ describe("translate function", function() {
     let result = translate("apple");
     expect(result).toBe("apple");
   });
-  it("If a word starts with a vowel, just add “way” onto the ending", function() {
+  it("If a word starts with a vowel (a), just add “way” onto the ending", function() {
     let result = translate("apple");
     expect(result).toBe("appleway");
+  });
+  it("If a word starts with a vowel (e), just add “way” onto the ending", function() {
+    let result = translate("eat");
+    expect(result).toBe("eatway");
+  });
+  it("If a word starts with a vowel (i), just add “way” onto the ending", function() {
+    let result = translate("ice");
+    expect(result).toBe("iceway");
   });
 
   //
